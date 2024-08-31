72f806a4f42ac44df6c1cc2db37f4d182bbb10f7
vaggeliskls <vaggeliskls@turintech.ai>
2024-08-31 20:36:26 +0300

    feat: namespace finalizer script

---
+++ scripts/namespace-finalizer.sh
@@ -0,0 +1,3 @@
+# When pvs stuck on deleting you just need to edit the pvc object and remove finalizers object.
+# https://github.com/kubernetes/kubernetes/issues/69697
+sleep 30; kubectl get namespace $1 && kubectl get namespace $1 -o json | jq 'del(.spec.finalizers[0])' | kubectl replace --raw "/api/v1/namespaces/$1/finalize" -f -
