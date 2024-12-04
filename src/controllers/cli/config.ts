d68030a484fa49e8b7d82b4dc844e42d8e55a8a2
vaggeliskls <vaggeliskls@turintech.ai>
2024-12-04 19:12:35 +0200

    feat: config info nodes and skip tls

--- src/controllers/cli/config.ts
+++ src/controllers/cli/config.ts
@@ -5,6 +5,7 @@ import { cliOutput, executor } from "../../shared/cli";
 import { actionRunner } from "../../shared/errors";
 import * as deployer from "../deployer";
 import * as k8s from "../kubernetes";
+import * as system from "../system";
 
 export function configCli(): Command {
   // CONFIG
@@ -14,10 +15,78 @@ export function configCli(): Command {
   configCli
     .command("cluster")
     .description("Select default cluster context")
+    .option("--skip-tls", "Skip TLS verify for the cluster", false)
+    .option("--tls", "Enforce TLS verify for the cluster", false)
     .action(
       actionRunner(async (options: any) => {
-        await k8s.selectClusterContext(options?.cluster);
+        await k8s.selectClusterContext();
+        if (options?.skipTls || options?.tls) {
+          await k8s.setKubeConfigSkipTlsVerify(options?.skipTls);
+          cliOutput.success({
+            title: `The insecure-skip-tls-verify=${options?.skipTls} updated to kube config`,
+          });
+        }
         cliOutput.success({ title: "The default cluster change completed" });
+      })
+    );
+
+  configCli
+    .command("info")
+    .description("Print cluster information")
+    .option("--env <text>", "Force environment by name")
+    .option("--change-env", "Change environment selection", false)
+    .action(
+      actionRunner(async (options: any) => {
+        const selectedEnv = await deployer.selectEnvironment(options.changeEnv, options?.env);
+        const envData = deployer.getMergedEnvironment(selectedEnv);
+        await deployer.deploymentInfo(envData);
+        const defaultContext = k8s.getDefaultClusterName();
+        const clusterDetails = k8s.getClusterInfo(defaultContext);
+        const table = new Table({
+          colWidths: [15, 90],
+        });
+        const toGreen = cliOutput.colors.green;
+        table.push({ [`${toGreen("Home")}`]: system.getHomeDirectory() });
+        table.push({ [`${toGreen("Kubeconfig")}`]: k8s.getKubeConfigPath() ?? "N/A" });
+        table.push({ [`${toGreen("Kubernetes")}`]: (await k8s.getKubernetesVersion()) ?? "N/A" });
+        table.push({ [`${toGreen("Openshift")}`]: (await k8s.isOpenshift()) ?? "N/A" });
+        for (const [key, value] of Object.entries(clusterDetails)) {
+          table.push({ [`${toGreen(key)}`]: value ?? "-" });
+        }
+        cliOutput.log({ title: "Machine Information", bodyLines: [table.toString()] });
+        // Print packages version information
+        await deployer.printPackagesInfo();
+      })
+    );
+
+  configCli
+    .command("nodes")
+    .description("Print nodes information")
+    .action(
+      actionRunner(async () => {
+        const nodes = await k8s.getNodeDetails();
+        let index = 1;
+        nodes.forEach((node: any) => {
+          const table = new Table();
+          const toGreen = cliOutput.colors.green;
+          const memory = node?.status?.capacity?.memory;
+          const disk = node?.status?.capacity["ephemeral-storage"];
+          table.push({ [`${toGreen("CPU")}`]: node?.status?.capacity?.cpu || "N/A" });
+          table.push({
+            [`${toGreen("RAM")}`]: memory ? system.formatBytes(memory, "KiB") : "N/A",
+          });
+          table.push({
+            [`${toGreen("DISK")}`]: disk ? system.formatBytes(disk, "KiB") : "N/A",
+          });
+          table.push({
+            [`${toGreen("GPU")}`]: node?.status?.capacity["nvidia.com/gpu"] || "N/A",
+          });
+          cliOutput.log({
+            title: `Node ${index}: ${node?.metadata?.name}`,
+            bodyLines: [table.toString()],
+          });
+          index++;
+        });
       })
     );
 
@@ -38,6 +107,7 @@ export function configCli(): Command {
     .description("Create registry secret for authentication")
     .option("--env <text>", "Force environment by name")
     .option("--change-env", "Change environment selection", false)
+    .option("--create-namespace", "Create namespace before add secret", false)
     .addOption(new Option("-r, --registry <text>", "The registry url").makeOptionMandatory(true))
     .addOption(new Option("-n, --namespace <text>", "Namespace to add the secret"))
     .addOption(
@@ -54,7 +124,9 @@ export function configCli(): Command {
         const selectedEnv = await deployer.selectEnvironment(options.changeEnv, options?.env);
         const envData = deployer.getMergedEnvironment(selectedEnv);
         const namespace = options?.namespace ?? envData.namespace;
-        await k8s.createNamespace(namespace);
+        if (options?.createNamespace) {
+          await k8s.createNamespace(namespace);
+        }
         await k8s.deleteSecret(options?.name, namespace);
         await executor.runCommandAsync(
           `kubectl create secret --namespace ${namespace} docker-registry ${options?.name} --docker-server=${options?.registry} --docker-username=${options?.username} --docker-password=${options?.password}`
