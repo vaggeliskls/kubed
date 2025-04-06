d1a9620ba539cae8300bc49e466c441001987f48
vaggeliskls <vaggeliskls@turintech.ai>
2025-04-07 02:32:27 +0300

    feat: clean typescript eslint checks

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,41 @@
-import eslint from "@typescript-eslint/parser";
-import { defineConfig } from "eslint/config";
+// @ts-check
+import eslint from "@eslint/js";
+import jestPlugin from "eslint-plugin-jest";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
+export default tseslint.config(
   {
-    ignores: ["node_modules", "dist"],
-    files: ["./src/**/*.ts", "./src/**/*.tsx"],
-    languageOptions: {
-      parser: eslint,
+    ignores: ["**/assets/**", "**/bin/**", "**/dist/**", "**/node_modules/**"],
+  },
+  eslint.configs.recommended,
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      jest: jestPlugin,
     },
-    plugins: {},
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        allowDefaultProject: true,
+      },
+    },
+    ignores: ["src/**/*.spec.ts", "src/**/*.test.ts"],
+    files: ["./src/**/*.ts", "./src/**/*.tsx"],
     rules: {
       "no-console": "error",
       "no-unused-vars": "off",
+      "no-undef": "off",
     },
   },
-]);
+  {
+    // disable type-aware linting on JS files
+    files: ["**/*.js"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  {
+    // enable jest rules on test files
+    files: ["test/**", "src/**/*.spec.ts", "src/**/*.test.ts"],
+    extends: [jestPlugin.configs["flat/recommended"]],
+  }
+);
